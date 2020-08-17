6182000d179733f0383f3e318edb27ad3aaeb530
Elyorbek Ibrokhimov <eliyoribek@gmail.com>
2020-08-17 14:39:12 +0900

    Add Bag using LinkedListBag

--- tech/ibrokhimov/datastructures/bag/LinkedListBag.java
+++ tech/ibrokhimov/datastructures/bag/LinkedListBag.java
@@ -1,4 +1,54 @@
 package tech.ibrokhimov.datastructures.bag;
 
-public class LinkedListBag {
+import java.util.Iterator;
+
+public class LinkedListBag<Item> implements Bag<Item> {
+
+    private Node first;
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    @Override
+    public void add(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        N++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    @Override
+    public int size() {
+        return N;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
 }
