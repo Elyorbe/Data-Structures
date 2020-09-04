f87efd023a26fa9fa24a25665cb8835849ac256a
Elyorbek Ibrokhimov <eliyoribek@gmail.com>
2020-09-04 22:42:25 +0900

    Add insertion sort

--- tech/ibrokhimov/algorithms/sorting/ElementarySorts.java
+++ tech/ibrokhimov/algorithms/sorting/ElementarySorts.java
@@ -16,4 +16,18 @@ public class ElementarySorts  {
             ArrayUtils.swapInt(array, i, min);
         }
     }
+
+    public static void insertionSort(int[] array) {
+        if(array == null)
+            return;
+        for(int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i-1;
+            while(j >=0 && array[j] > key) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = key;
+        }
+    }
 }
